fd2b9ca95c092b58e15ede212d83b6cc3c2e4aa9
sammanomar <>
2023-06-15 23:12:04 +0000

    update header.html navigation contents, define blogs function at views.py and update blogs url at urls.py

--- flight/urls.py
+++ flight/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', home, name='home'),
+    path('blogs/', blogs, name='blogs'),
 ]
--- flight/views.py
+++ flight/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def blogs(request):
+    return render(request, 'blogs.html')    
